aebdb11921e095e969d014d7fe2231c3f9771b02
luc4sleite <lucahleite@gmail.com>
2023-02-24 17:23:13 -0300

    adicionando um novo console log

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
+
+// adicionando um novo código
+console.log("adicionando um novo código")
